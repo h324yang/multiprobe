b79567d464060b982e82c54419c3bcceae08371e
Ralph Tang <r33tang@uwaterloo.ca>
2019-09-12 19:48:07 -0400

    Remove BERT hook type option
    - Make BERT computation script save all relevant embeddings, since there's no performance gain from specifying anyways.

--- multiprobe/run/compute_bert_output.py
+++ multiprobe/run/compute_bert_output.py
@@ -29,33 +29,36 @@ def main():
     parser.add_argument('--dataset-file', type=str, required=True)
     parser.add_argument('--bert-model', type=str, default='bert-base-multilingual-uncased')
     parser.add_argument('--batch-size', type=int, default=16)
-    parser.add_argument('--layers', type=int, nargs='+', default=[-1, 0, 2, 4, 6, 8])
-    parser.add_argument('--hooks', type=str, nargs='+', default=['attention'], choices=['attention', 'output', 'word-embeddings'])
+    parser.add_argument('--layers', type=int, nargs='+', default=list(range(12)))
     args = parser.parse_args()
 
     model = BertModel.from_pretrained(args.bert_model).cuda()
     model.eval()
     tokenizer = BertTokenizer.from_pretrained(args.bert_model)
 
-    hooks = [BundleAveragingHook(reduce='word-embeddings' not in args.hooks) for _ in args.layers]
-    for layer_idx, hook in zip(args.layers, hooks):
-        if 'output' in args.hooks:
-            hook_bert_layer_output(model, layer_idx, hook)
-        if 'attention' in args.hooks:
-            hook_bert_layer_attn(model, layer_idx, hook)
-        if 'word-embeddings' in args.hooks:
-            hook_bert_word_embeddings(model, hook)
+    hooks = []
+    for idx in args.layers:
+        output_hook = BundleAveragingHook(reduce=True)
+        attn_hook = BundleAveragingHook(reduce=True)
+        hook_bert_layer_attn(model, idx, output_hook)
+        hook_bert_layer_attn(model, idx, attn_hook)
+        hooks.append((f'output-l{idx}', output_hook))
+        hooks.append((f'attn-l{idx}', attn_hook))
+    word_embed_hook = BundleAveragingHook(reduce=False)
+    hooks.append((f'word-emb', word_embed_hook))
+    hook_bert_word_embeddings(model, word_embed_hook)
+
     df = pd.read_csv(args.dataset_file, sep='\t', quoting=3)
     for data in tqdm(list(chunk(list(df.itertuples()), args.batch_size))):
         _, languages, sentences = list(zip(*data))
         bundle = SingleInputBundle(list(map(str.split, sentences)), tokenizer.vocab)
         bundle.cuda()
-        for hook in hooks:
+        for _, hook in hooks:
             hook.bundle = bundle
         with torch.no_grad():
             model(bundle.token_ids, bundle.segment_ids, bundle.input_mask)
-    for layer_idx, hook in zip(args.layers, hooks):
-        torch.save(torch.cat(hook.data), f'{args.output_file_prefix}-l{layer_idx}.pt')
+    for name, hook in hooks:
+        torch.save(torch.cat(hook.data), f'{args.output_file_prefix}-{name}.pt')
 
 
 if __name__ == '__main__':
